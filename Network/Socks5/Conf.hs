280b0b0c6d71f4e0b2759e6305676b107aacb4f7
Vincent Hanquez <vincent@snarc.org>
2013-02-16 10:04:32 +0000

    add a way to define a default SocksConf from a SockAddr

--- Network/Socks5/Conf.hs
+++ Network/Socks5/Conf.hs
@@ -4,6 +4,7 @@ module Network.Socks5.Conf
     , socksHost
     , socksPort
     , defaultSocksConf 
+    , defaultSocksConfFromSockAddr
     ) where
 
 import Network.Socket
@@ -33,7 +34,16 @@ socksPort = undefined
 
 -- | defaultSocksConf create a new record, making sure
 -- API remains compatible when the record is extended.
-defaultSocksConf host port = SocksConf socksServer SocksVer5
-    where socksServer = SocksServerAddress haddr port
-          haddr       = SocksAddrDomainName $ BC.pack host
+defaultSocksConf host port = SocksConf server SocksVer5
+    where server = SocksServerAddress haddr port
+          haddr  = SocksAddrDomainName $ BC.pack host
 
+-- | same as defaultSocksConf except the server address is determined from a 'SockAddr'
+--
+-- A unix SockAddr will raises an error. Only Inet and Inet6 types supported
+defaultSocksConfFromSockAddr sockaddr = SocksConf server SocksVer5
+    where server       = SocksServerAddress haddr port
+          (haddr,port) = case sockaddr of
+                             SockAddrInet p h      -> (SocksAddrIPV4 h, p)
+                             SockAddrInet6 p _ h _ -> (SocksAddrIPV6 h, p)
+                             _                     -> error "unsupported unix sockaddr type"
